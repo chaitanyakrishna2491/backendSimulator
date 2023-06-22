c9e7f77e22211e38e5362c093d38960a57e46ee6
DESKTOP-EO6RKB1\Dell <chaitanya@bsconsultants.us>
2023-06-22 23:50:01 +0530

    making credit card unique

--- backend-simulator/src/credit-card/entities/CreditCard.entity.ts
+++ backend-simulator/src/credit-card/entities/CreditCard.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
 export class CreditCard {
     @PrimaryGeneratedColumn() id:string;
-    @Column() card_Number:string;
+    @Column({unique: true}) card_Number:string;
     @Column() user_Id?:number;
     @Column() company?:string;
     @Column() card_holder_name?:string;
